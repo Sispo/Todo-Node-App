318730c522944f4f23c27cc54204a3fec4748067
Tymofii Dolenko <tymofii@dolenko.com>
2017-04-21 20:46:20 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,6 +32,24 @@ app.get('/todos',(req,res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (ObjectID.isValid(id)) {
+
+    Todo.findById(id).then((todo) => {
+      if (todo) {
+        res.send(todo);
+      } else {
+        res.status(404).send();
+      }
+    }).catch((e) => res.status(400).send());
+
+  } else {
+    res.status(404).send();
+  }
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
